d32d143126532f400b0fab4d0e05870c5f50cc3f
Abiodun <abiodunfaboyode007@gmail.com>
2024-08-23 17:37:36 +0100

    Update works.js

--- src/data/works.js
+++ src/data/works.js
@@ -9,14 +9,14 @@ export default [
 	{
 		title: "Agricversity Landing page",
 		imgUrl:
-			'https://img.com/gallery/farrrrrm-trackerr-dj3MqmA',
+			'https://github.com/AbiodunCreatives/imagehost/blob/main/Screenshot_20240823-032900_Chrome.jpg',
 		tech: ['Html', 'Tailwind', 'Vercel'],
 		workUrl: 'https://agricversity.vercel.app/',
 	},
 	{
 		title: "Roamio Hailing Services",
 		imgUrl:
-			'https://imgur.com/a/YgE3sCf',
+			'https://imgur.com/a/XEiyoxb',
 		tech: ['Canva', 'photoshop'],
 		//workUrl: 'https://github.com/ShaifArfan/AYANs-portfolio',
 	},
